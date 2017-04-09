28e3e9aa47099401f602294ac802863b0d5ff0ed
KamilKepinski <mlody758@gmail.com>
2017-04-09 15:48:29 +0200

    Zmiana wyświetlanego tekstu

---
+++ PTM-HelloGit-Kepinski/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string tekst = "Ja, Kamil Kepinski, jestem pierwszym autorem tego programu";
+	cout << tekst << endl;
+	system("pause");
+	return 0;
+
+
+
+}
